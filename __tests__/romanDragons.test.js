d84eb456a619401f7e9779851acacf9d26b94584
Emma Gerigscott <elgerig@gmail.com>
2023-06-20 11:32:23 -0700

    passing tests
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- __tests__/romanDragons.test.js
+++ __tests__/romanDragons.test.js
@@ -27,5 +27,21 @@ describe('dragonizer', () => {
 
   test('should return "VI" if 6 is put in', () => {
     expect(dragonizer(6)).toEqual("VI");
+  })
+
+  test('should return "VII" if 7 is put in', () => {
+    expect(dragonizer(7)).toEqual("VII");
+  })
+
+  test('should return "VIII" if 8 is put in', () => {
+    expect(dragonizer(8)).toEqual("VIII");
+  })
+
+  test('should return "IX" if 9 is put in', () => {
+    expect(dragonizer(9)).toEqual("IX");
+  })
+
+  test('should return "X" if 10 is put in', () => {
+    expect(dragonizer(10)).toEqual("X");
   });
 });
--- src/romanDragons.js
+++ src/romanDragons.js
@@ -6,7 +6,7 @@ export default function dragonizer(num) {
   if (num < 5) {
     if (num == 4) {
       return dragonizer(num - 3) + "V";
-    } else if (num <= 3) {
+    } else {
       return dragonizer(num - 1) + "I";
     }
   } else if (num >= 5 && num < 10) {
@@ -16,7 +16,11 @@ export default function dragonizer(num) {
     } else if ((num % 5) == 0) {
       console.log("FARTT");
       return dragonizer(num % 5) + "V";
+    } else {
+      return dragonizer(num - 8) + "X";
     }
+  } else if (num >= 10) {
+    return dragonizer(num % 5) + "X";
   }
 }
 

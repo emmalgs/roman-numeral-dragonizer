f0a84490865fc0f6af7990a0d34ad417c9a5d794
Emma Gerigscott <elgerig@gmail.com>
2023-06-20 16:04:22 -0700

    yeah fart

--- src/romanDragons.js
+++ src/romanDragons.js
@@ -2,28 +2,20 @@ export default function dragonizer(num) {
   if (num === 0) {
     return "";
   }
-  if (num < 5) {
-    if (num == 4) {
-      return dragonizer(num - 3) + "V";
-    } else {
-      return dragonizer(num - 1) + "I";
-    }
-  } else if (num >= 5 && num < 10) {
+  if (num < 15)
+  {
     if ((num % 5 < 4) && (num % 5 >= 1)) {
       return dragonizer(num - 1) + "I";
-    } else if ((num % 5) == 0) {
-      return dragonizer(num % 5) + "V";
-    } else {
+    } else if (parseInt(num.toString().slice(-1)) == 4) {
+      return dragonizer(num - 3) + "V";
+    } else if ((num == 5)) {
+      return dragonizer(num - 5) + "V";
+    } else if (parseInt(num.toString().slice(-1)) == 9) {
       return dragonizer(num - 8) + "X";
+    } else if (num % 10 == 0) {
+      return dragonizer(num - 10) + "X";
     }
-  } else if (num >= 10 && num < 15) {
-    if ((num % 5 < 4) && (num % 5 >= 1)) {
-      return dragonizer(num - 1) + "I";
-    } else if (num % 5 == 4) {
-      return dragonizer(num - 3) + "V";
-    } 
-    return dragonizer(num % 5) + "X";
-  } else if (num >= 15) {
-    return dragonizer(num - 5) + "V";
+  } else {
+    return;
   }
 }
